24aec8eef6e86cc6e2eddddc8359f341f3115bbf
ed-breno <breno.henrique@empreenderdinheiro.com>
2024-12-02 21:42:01 -0300

    :feat: connect to postgress database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Client } from './entities/client.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 3306,
-      username: 'root',
+      port: 5432,
+      username: 'postgres',
       password: 'admin',
       database: 'teddylist',
-      entities: [],
+      entities: [Client],
       synchronize: true,
     }),
   ],
